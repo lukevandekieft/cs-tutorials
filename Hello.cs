472a5c070c9f2ac1aad74680c3d614a2a3e622b3
lukevandekieft <vandekie@gmail.com>
2019-09-22 16:26:16 -0700

    Create functioning 'hello world' in C#.

---
+++ Hello.cs
@@ -0,0 +1,10 @@
+// Our directives: this tells the code what it needs to function
+using System; // Needed for Console
+
+class Program {
+  // Entry Point
+  static void Main()
+  {
+    Console.WriteLine("Hello World");
+  }
+}
