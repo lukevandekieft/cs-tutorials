42269ebd47602e57b7835c47a9f241347991043a
lukevandekieft <vandekie@gmail.com>
2019-09-29 13:57:29 -0700

    Create additional testing scripts.

---
+++ OldScripts/HelloWorld.cs
@@ -0,0 +1,16 @@
+// // Our directives: this tells the code what it needs to function
+// using System; // Needed for Console
+//
+// class Program {
+//   // Defined function for use in Entry Point
+//   static void DoubleNumber() {
+//     int yourNumber = int.Parse(Console.ReadLine()) * 2;
+//     Console.WriteLine("I doubled your number to " + yourNumber);
+//   }
+//   // Entry Point
+//   static void Main() {
+//     Console.WriteLine("Give me a number and I will double it for you!");
+//     DoubleNumber();
+//
+//   }
+// }
---
+++ OldScripts/NumberLogic.cs
@@ -0,0 +1,17 @@
+// // Our directives: this tells the code what it needs to function
+// using System; // Needed for Console
+//
+// class RatedR {
+//
+//   // Entry Point
+//   static void Main() {
+//     Console.WriteLine("How old are you?");
+//     int userAge = int.Parse(Console.ReadLine());
+//
+//     if (userAge >= 17) {
+//       Console.WriteLine("You can see the movie!");
+//     } else {
+//       Console.WriteLine("I'm sorry, you are too young to see the movie.");
+//     }
+//   }
+// }
