044cb6c0995cff4423cbc275fc7358ca7726c1e4
Serverless Guru <dev@serverlessguru.com>
2019-10-04 12:03:44 -0700

    Create dictionary function.

--- OldScripts/Boolean.cs
+++ OldScripts/Boolean.cs
@@ -4,6 +4,21 @@ class FirstLetterChecker
 {
   static void Main()
   {
+    // Example arrays
+    string[] fruits = { "apples", "bananas", "oranges", "grapes" };
+    int[] primeNumbers = { 2, 3, 5, 7, 11 };
+
+    // Dictionary & example calls
+    Dictionary<string, string> myDictionary = new Dictionary<string, string>() { {"A", "apple"}, {"B", "bear"} };
+    // > myDictionary
+    // { {"A", "apple"}, {"B", "bear"} }
+    //
+    // > myDictionary["A"]
+    // "apple"
+    //
+    // > myDictionary["B"]
+    // "bear"
+
     Console.WriteLine("What is your name?");
     string userName = Console.ReadLine();
     if (userName.StartsWith("Z"))
